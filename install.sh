4d6c17495543b93f889ebedb899d95f6357c19bc
Ahmad Nassri <ahmad@ahmadnassri.com>
2015-08-06 15:36:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 URL="https://github.com/Mashape/kong/releases/download/VERSION/kong-VERSION.trusty_all.deb"
 LATEST=$(http https://api.github.com/repos/mashape/kong/releases/latest | jq -r .tag_name)
 wget ${URL//VERSION/$LATEST}
-dpkg -i kong-*.trusty_all.deb
+dpkg -i kong-$LATEST.trusty_all.deb
